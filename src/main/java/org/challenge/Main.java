8f7f9f23eed68b98601ac054d79d7a8dbedee7ee
Andrea Lazzaretti <andrealazzaretti@gmail.com>
2023-04-08 14:38:41 +0200

    Upload sample data, add Path validation in Main

--- src/main/java/org/challenge/Main.java
+++ src/main/java/org/challenge/Main.java
@@ -5,6 +5,7 @@ import static java.lang.Integer.parseInt;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import java.io.*;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.stream.Collectors;
@@ -15,11 +16,12 @@ import org.challenge.model.output.OutputLocalMaxima;
 public class Main {
   public static void main(String[] args) {
     if (args.length != 2) {
-      throw new IllegalArgumentException("This program expects two arguments");
+      throw new IllegalArgumentException("This program expects exactly two arguments");
     }
-    var resourceName = args[0];
+    var filePath = Paths.get(args[0]);
     var requestedNumberOfMaxima = parseInt(args[1]);
-    try (Reader reader = new InputStreamReader(new FileInputStream(resourceName))) {
+
+    try (Reader reader = new InputStreamReader(new FileInputStream(filePath.toFile()))) {
       Gson gson = new GsonBuilder().setPrettyPrinting().create();
       var meshJsonData = gson.fromJson(reader, MeshJson.class);
       var mesh = meshJsonData.toExplorableMesh();
